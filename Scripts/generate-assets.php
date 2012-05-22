603fd8e92dc84e5b4af3a63dbf4870c1c624ae8e
Marc Neuhaus <mneuhaus@famelo.com>
2012-05-23 00:17:14 +0200

    Initial Commit with current Bootstrap Git master

---
+++ Scripts/generate-assets.php
@@ -0,0 +1,37 @@
+<?php
+
+$cssBundles = array(
+	"Bootstrap" => "Public/less/bootstrap.less",
+	"Bootstrap-Responsive" => "Public/less/responsive.less"
+);
+
+$path = getcwd();
+
+$assets = 'Bundles:
+  CSS:';
+foreach ($cssBundles as $name => $file) {
+	$content = file_get_contents($path . "/Resources/" . $file);
+	preg_match_all('/@import.*"(.+)"/', $content, $matches);
+	foreach ($matches[1] as $key => $match) {
+		$content = str_replace($matches[0][$key], "- " . dirname($file) . "/" . $match, $content);
+	}
+	$content = str_replace("/*", "#", $content);
+	$content = str_replace(" */", "#", $content);
+	$content = str_replace(" *", "#", $content);
+	$content = str_replace("//", "#", $content);
+	$assets.= prependText("\n" . $name . ":", 4) . "\n";
+	$assets.= prependText($content, 6);
+
+}
+
+function prependText($text, $spaces = 2) {
+	$lines = explode("\n", $text);
+	foreach ($lines as $key => $line) {
+		$lines[$key] = str_repeat(" ", $spaces) . $line;
+	}
+	return implode("\n", $lines);
+}
+
+file_put_contents("Configuration/Assets.yaml", $assets);
+
+?>
